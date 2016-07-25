730fe80559492c01daec781f14f218e2d469dc53
Umar Iqbal <uiqbal@iai.uni-bonn.de>
2016-07-25 20:42:49 +0200

    script to download required data

---
+++ get_data.sh
@@ -0,0 +1,9 @@
+# get normalized 3D pose databases
+#wget http://pages.iai.uni-bonn.de/iqbal_umar/ds3dpose/data/Data.tar.gz
+#tar -zxvf Data.tar.gz
+#rm Data.tar.gz
+
+
+# get regressors for 2D pose estimate
+wget http://pages.iai.uni-bonn.de/iqbal_umar/ds3dpose/data/regressors.tar.gz
+tar -zxvf regressors.tar.gz ./MEX/src
